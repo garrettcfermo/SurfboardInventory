cf8179a92fd715f45b28ec3c98f215b57ad98c0d
Garrett Fermo <garrettcfermo@gmail.com>
2018-11-06 17:19:27 -0800

    update server

---
+++ config/config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  "development": {
+    "username": "root",
+    "password": "rootroot",
+    "database": "surfboard_db",
+    "host": "127.0.0.1",
+    "port": 3306,
+    "dialect": "mysql"
+  },
+  "test": {
+    "username": "root",
+    "password": "rootroot",
+    "database": "surfboard_db",
+    "host": "127.0.0.1",
+    "port": 3306,
+    "dialect": "mysql"
+  },
+  "production": {
+    "username": process.env.DB_USERNAME,
+    "password": process.env.DB_PASSWORD,
+    "database": process.env.DB_NAME,
+    "host": process.env.DB_HOSTNAME,
+    "dialect": "mysql",
+    "use_env_variable": "DATABASE_URL"
+  }
+}
