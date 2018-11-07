44b0841b0ab7dc2a346ff43ff63207620ba96d2b
Garrett Fermo <garrettcfermo@gmail.com>
2018-11-06 17:27:41 -0800

    update server

--- config/config.js
+++ config/config.js
@@ -16,11 +16,7 @@ module.exports = {
     "dialect": "mysql"
   },
   "production": {
-    "username": process.env.DB_USERNAME,
-    "password": process.env.DB_PASSWORD,
-    "database": process.env.DB_NAME,
-    "host": process.env.DB_HOSTNAME,
     "dialect": "mysql",
-    "use_env_variable": "DATABASE_URL"
+    "use_env_variable": "JAWSDB_URL"
   }
 }
